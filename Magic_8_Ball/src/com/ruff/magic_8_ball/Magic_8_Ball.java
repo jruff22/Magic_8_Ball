9912d09c0a96d0b1588c7b0476f3fcd0e7bb455b
Joe <Joe@LAPTOP-TTJOQJ4M>
2020-06-30 11:45:17 -0500

    Initial commit

---
+++ Magic_8_Ball/src/com/ruff/magic_8_ball/Magic_8_Ball.java
@@ -0,0 +1,5 @@
+package com.ruff.magic_8_ball;
+
+public class Magic_8_Ball {
+
+}
---
+++ Magic_8_Ball/test/com/ruff/magic_8_ball/Magic_8_BallTest.java
@@ -0,0 +1,22 @@
+package com.ruff.magic_8_ball;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class Magic_8_BallTest {
+	
+	public static Magic_8_Ball ball;
+	
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
